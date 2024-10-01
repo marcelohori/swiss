5bce2542603eb9104ff47f1091e49007ec2decd3
MARCELO HORI <marcelo.hori@viavarejo.com.br>
2024-10-01 11:34:47 -0300

    Add project files.

---
+++ TradeCategorization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TradeCategorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leitura da data de referência no formato mm/dd/yyyy
+            DateTime referenceDate = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+            // Leitura do número de negociações
+            int n = int.Parse(Console.ReadLine());
+
+            // Lista para armazenar as negociações
+            List<ITrade> trades = new List<ITrade>();
+
+            // Leitura das negociações
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                double value = double.Parse(input[0]);
+                string sector = input[1];
+                DateTime nextPaymentDate = DateTime.ParseExact(input[2], "MM/dd/yyyy", CultureInfo.InvariantCulture); // Formato mm/dd/yyyy
+
+                trades.Add(new Trade(value, sector, nextPaymentDate));
+            }
+
+            // Instancia o categorizador
+            TradeCategorizer categorizer = new TradeCategorizer();
+
+            // Classifica e imprime o resultado para cada negociação
+            foreach (var trade in trades)
+            {
+                Console.WriteLine(categorizer.Categorize(trade, referenceDate));
+            }
+        }
+    }
+}
---
+++ TradeCategorization/TradeCategorization.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCategorization
+{
+    public interface ITrade
+    {
+        double Value { get; }
+        string ClientSector { get; }
+        DateTime NextPaymentDate { get; }
+    }
+
+    public class Trade : ITrade
+    {
+        public double Value { get; private set; }
+        public string ClientSector { get; private set; }
+        public DateTime NextPaymentDate { get; private set; }
+
+        public Trade(double value, string clientSector, DateTime nextPaymentDate)
+        {
+            Value = value;
+            ClientSector = clientSector;
+            NextPaymentDate = nextPaymentDate;
+        }
+    }
+
+    public interface ICategory
+    {
+        string Name { get; }
+        bool IsMatch(ITrade trade, DateTime referenceDate);
+    }
+
+    public class ExpiredCategory : ICategory
+    {
+        public string Name => "EXPIRED";
+
+        public bool IsMatch(ITrade trade, DateTime referenceDate)
+        {
+            return (referenceDate - trade.NextPaymentDate).Days > 30;
+        }
+    }
+
+    public class HighRiskCategory : ICategory
+    {
+        public string Name => "HIGHRISK";
+
+        public bool IsMatch(ITrade trade, DateTime referenceDate)
+        {
+            return trade.Value > 1000000 && trade.ClientSector == "Private";
+        }
+    }
+
+    public class MediumRiskCategory : ICategory
+    {
+        public string Name => "MEDIUMRISK";
+
+        public bool IsMatch(ITrade trade, DateTime referenceDate)
+        {
+            return trade.Value > 1000000 && trade.ClientSector == "Public";
+        }
+    }
+
+    public class TradeCategorizer
+    {
+        private readonly List<ICategory> _categories;
+
+        public TradeCategorizer()
+        {
+            _categories = new List<ICategory>
+            {
+                new ExpiredCategory(),
+                new HighRiskCategory(),
+                new MediumRiskCategory()
+            };
+        }
+
+        public string Categorize(ITrade trade, DateTime referenceDate)
+        {
+            foreach (var category in _categories)
+            {
+                if (category.IsMatch(trade, referenceDate))
+                {
+                    return category.Name;
+                }
+            }
+            return "UNCATEGORIZED";
+        }
+    }
+}
